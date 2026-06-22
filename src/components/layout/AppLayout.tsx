908c09dd80e579916e4f16c035d124cf1a1d45d6
ASHUTOSH <ashutosh@ASHUTOSHs-MacBook-Air.local>
2026-06-23 00:20:37 +0530

    fixed children type in AppLayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,12 @@
 import Sidebar from "./Sidebar/Sidebar";
+import type { ReactNode } from "react";
 
+type AppLayoutProps = {
+    children: ReactNode;
+}
 
-export default function AppLayout({ children }) {
+export default function AppLayout({ children }: AppLayoutProps) {
+    
     return (
         <div>
             <Sidebar />
